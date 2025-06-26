614797a2f977307376ed93a050adc5dbdae1b6a3
jony <ravshanovjony7@gmail.com>
2025-06-26 15:36:37 +0500

    16-dars Formlar bilan ishlash

--- src/components/movies-add-form/movies-add-form.js
+++ src/components/movies-add-form/movies-add-form.js
@@ -1,18 +1,53 @@
-import '../movies-add-form/movies-add-form.css'
+import "../movies-add-form/movies-add-form.css";
+import { Component } from "react";
 
-const MoveAddForm = () => {
-  return (
-    <div className='movies-add-form'>
+class MoveAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      name: "",
+      views: "",
+    };
+  }
+
+  changeHendlarInput = (e) => {
+    console.log(e.target.value);
+    
+    this.setState({
+      [e.target.name]: e.target.value,
+      [e.target.views]: e.target.value
+    })
+  };
+
+  render = () => {
+    const {name, views} = this.state
+
+    return (
+      <div className="movies-add-form">
         <h3>Yangi kino qoshish</h3>
-        <form action= "" className='add-form d-flex'>
-            <input type="text" className='form-control new-post-label' placeholder='Qanday kino? '/>
-            <input type="number" className='form-control new-post-label' placeholder='Nechi marotaba korilgan?'/>
-            <button type='button' className='btn btn-outline-dark'>
-                Qoshish
-            </button>
+        <form action="" className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Qanday kino? "
+            onChange={this.changeHendlarInput}
+            name="name"
+            value={name}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="Nechi marotaba korilgan?"
+            onChange={this.changeHendlarInput}
+            name="views"
+            value={views}
+          />
+          <button type="button" className="btn btn-outline-dark">
+            Qoshish
+          </button>
         </form>
-    </div>
-  )
+      </div>
+    );
+  };
 }
-
-export default MoveAddForm
+export default MoveAddForm;
